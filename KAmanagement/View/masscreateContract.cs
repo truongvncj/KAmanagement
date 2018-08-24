7b0087bfe512f2913ec50a5b5b901f438885494d
truongvncj <truongvncj@gmail.com>
2018-08-24 10:10:54 +0700

    cm
    cm

--- KAmanagement/View/masscreateContract.cs
+++ KAmanagement/View/masscreateContract.cs
@@ -494,6 +494,7 @@ namespace KAmanagement.View
             #region  //Sai Programe type
             List<string> listprogr = new List<string>();
             listprogr.Add("FRE");
+            listprogr.Add("POS");
             //     listprogr.Add("DASANI");
 
             var mlist14 = from p in dc.tbl_tempcontractsdatadetails
@@ -503,7 +504,7 @@ namespace KAmanagement.View
             {
                 foreach (var item in mlist14)
                 {
-                    item.StatusNote = "Contract type sai ! hiện có FRE ";
+                    item.StatusNote = "Contract type sai ! hiện chỉ có 2 loại FRE và POS ";
                     dc.SubmitChanges();
                 }
                 checkberore = false;
