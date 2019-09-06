28f91e7f1b50e09855ee7b112418bb05e9bab83d
truongvncj <truongvncj@gmail.com>
2019-09-06 13:00:43 +0700

    cm

---
+++ KAmanagement/View/kAselectregiontodelete.Designer.cs
@@ -0,0 +1,162 @@
+﻿namespace KAmanagement.View
+{
+    partial class kAselectregiontodelete
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(kAselectregiontodelete));
+            this.userchoice = new System.Windows.Forms.ComboBox();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.button3 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.cbfuctionchoice = new System.Windows.Forms.ComboBox();
+            this.button1 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // userchoice
+            // 
+            this.userchoice.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.userchoice.FormattingEnabled = true;
+            this.userchoice.ItemHeight = 13;
+            this.userchoice.Location = new System.Drawing.Point(78, 72);
+            this.userchoice.Name = "userchoice";
+            this.userchoice.Size = new System.Drawing.Size(118, 21);
+            this.userchoice.TabIndex = 2;
+            this.userchoice.SelectedIndexChanged += new System.EventHandler(this.comboBox1_SelectedIndexChanged);
+            this.userchoice.SelectedValueChanged += new System.EventHandler(this.userchoice_SelectedValueChanged);
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(12, 99);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(490, 330);
+            this.dataGridView1.TabIndex = 3;
+            this.dataGridView1.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView1_CellContentClick);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(391, 454);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(85, 21);
+            this.button3.TabIndex = 4;
+            this.button3.Text = "Delete";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(11, 76);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(62, 13);
+            this.label1.TabIndex = 6;
+            this.label1.Text = "Select User";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Perpetua Titling MT", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.ForeColor = System.Drawing.Color.Red;
+            this.label2.Location = new System.Drawing.Point(9, 9);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(265, 17);
+            this.label2.TabIndex = 7;
+            this.label2.Text = "Please select region to delete";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(11, 51);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(38, 13);
+            this.label3.TabIndex = 8;
+            this.label3.Text = "Set up";
+            // 
+            // cbfuctionchoice
+            // 
+            this.cbfuctionchoice.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.cbfuctionchoice.FormattingEnabled = true;
+            this.cbfuctionchoice.ItemHeight = 13;
+            this.cbfuctionchoice.Items.AddRange(new object[] {
+            "Contract Type View Setup",
+            "Sales Org View Setup"});
+            this.cbfuctionchoice.Location = new System.Drawing.Point(78, 46);
+            this.cbfuctionchoice.Name = "cbfuctionchoice";
+            this.cbfuctionchoice.Size = new System.Drawing.Size(118, 21);
+            this.cbfuctionchoice.TabIndex = 9;
+            this.cbfuctionchoice.SelectedIndexChanged += new System.EventHandler(this.cbfuctionchoice_SelectedIndexChanged);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(271, 454);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(85, 21);
+            this.button1.TabIndex = 10;
+            this.button1.Text = "Cancel";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // kAselectregiontodelete
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(514, 487);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.cbfuctionchoice);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.userchoice);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "kAselectregiontodelete";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Select region to delete";
+            this.Load += new System.EventHandler(this.kAuserRightsetup_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.ComboBox userchoice;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.ComboBox cbfuctionchoice;
+        private System.Windows.Forms.Button button1;
+    }
+}
